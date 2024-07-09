c6481f4477cec2c1bd457c8c819aecf3143ce664
shapna <shapnagangadharan035@gmail.com>
2024-07-09 14:23:28 +0530

    update

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'jimsha',
   database: 'employee',
 });
 
